3a1e0dd42037bd6e9bcc7aedcd0d211496ba17b7
Srinjan Ghosh <srinjanghosh11@gmail.com>
2020-08-14 16:16:17 +0530

    educator pages, carousel navigation buttons, calendar

--- js/index.js
+++ js/index.js
@@ -9,14 +9,55 @@ $(document).ready(function(){
 
   $('#sidebarCollapse').on('click', function () {
     $('#sidebar').toggleClass('active');
-    $('#collapse-icon').toggleClass('fa-chevron-circle-left fa-chevron-circle-right');
+    $('#collapse-icon').toggleClass('fa-angle-left fa-angle-right');
+    if($('#sidebar').hasClass("active")){
+      $('#sidebarCollapse').css('left', '-15px')
+    } else {
+      $('#sidebarCollapse').css('left', '230px')
+    }
     
   });
 
-  $('.owl-carousel').owlCarousel({
-    autoplay: true,
-    dots: true,
+  $('.main-content .owl-carousel').owlCarousel({
+    stagePadding: 50, //new
     loop: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ], //new
+    navContainer: '.main-content .custom-nav', //new
+    margin: 25,
+    responsive: {
+      0: {
+          items: 1
+      },
+      500: {
+          items: 2
+      },
+      900: {
+          items: 3
+      },
+      1200: {
+          items: 4
+      },
+      1600: {
+          items: 5
+      }
+    }
+  });
+
+  $('.main-content-2 .owl-carousel').owlCarousel({
+    stagePadding: 50, //new
+    loop:true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ], //new
+    navContainer: '.main-content-2 .custom-nav', //new
     margin: 25,
     responsive: {
       0: {
